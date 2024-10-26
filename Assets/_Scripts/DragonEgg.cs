85d893e1b28b799e74ac244b25b54b7ec897045f
ytkinroman <mc3heisenberg@gmail.com>
2024-10-26 12:43:18 +0500

    Upd script file
    - Обновил DragonEgg.cs

--- Assets/_Scripts/DragonEgg.cs
+++ Assets/_Scripts/DragonEgg.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DragonEgg : MonoBehaviour
 {
     public static float bottomY = -30f;
     public AudioSource audioSource;
-    
-    void Start()
+    public float fallSpeed = 9f;
+
+    private void Start()
     {
-        
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0, -fallSpeed, 0);
     }
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other)
+    {
         ParticleSystem ps = GetComponent<ParticleSystem>();
         var em = ps.emission;
         em.enabled = true;
@@ -25,9 +29,9 @@ public class DragonEgg : MonoBehaviour
         audioSource.Play();
     }
 
-        void Update()
+    private void Update ()
     {
-        if (transform.position.y < bottomY){
+        if (transform.position.y < bottomY) {
             Destroy(this.gameObject);
             DragonPicker apScript = Camera.main.GetComponent<DragonPicker>();
             apScript.DragonEggDestroyed();
